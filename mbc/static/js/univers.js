1fa436bca5158471026224048fe0247713f282b3
patsykakaz <patsykakaz@cloud.MBC>
2015-02-13 10:24:39 -0500

    Almost done with CMS integration (from iMac-27-MBC)

--- mbc/static/js/univers.js
+++ mbc/static/js/univers.js
@@ -5,14 +5,14 @@ $(document).ready(function(){
 	bannerHeight = banner.outerHeight();
 	$('#banner-titre').css('left', ($(document).outerWidth() - $('#banner-titre:first').width())/2);
 	$('#banner-titre').css('top', ($('#banner-wrapper').height() - $('#banner-titre:first').outerHeight())/2 -10);
-	$('#univers-presentation p:first').prepend('<i class="fa fa-long-arrow-right"></i> ');
+	$('#main p:first').prepend('<i class="fa fa-long-arrow-right"></i> ');
 });
 $(window).load(function(){
 	banner = $('#banner');
 	bannerHeight = banner.outerHeight();
 	$('#banner-titre').css('left', ($(document).outerWidth() - $('#banner-titre:first').width())/2);
 	$('#banner-titre').css('top', ($('#banner-wrapper').height() - $('#banner-titre:first').outerHeight())/2 -20); // Problème pour centrer le #banner-titre, pour le moment on shorcut avec le -20px...
-	$('#univers-presentation').css('margin-top', $('#banner-wrapper').outerHeight()+'px');
+	$('#main').css('max-width', $('.container-all-titres').outerWidth()).css('margin-top', $('#banner-wrapper').outerHeight()+'px');
 });
 
 $(window).resize(function(){
@@ -20,5 +20,6 @@ $(window).resize(function(){
 	bannerHeight = banner.outerHeight();
 	$('#banner-titre').css('left', ($(document).outerWidth() - $('#banner-titre:first').width())/2);
 	$('#banner-titre').css('top', ($('#banner-wrapper').height() - $('#banner-titre:first').outerHeight())/2 -20); // Problème pour centrer le #banner-titre, pour le moment on shorcut avec le -20px...
-	$('#univers-presentation').css('margin-top', $('#banner-wrapper').outerHeight()+'px');
+	$('#main').css('max-width', $('.container-all-titres').outerWidth()).css('margin-top', $('#banner-wrapper').outerHeight()+'px');
 });
+
