b1d332b133fd7d3db47d7178db3cfb251faca399
patsykakaz <patsykakaz@iMac-de-patsykakaz.local>
2015-02-16 16:02:15 +0100

    same as above

--- mbc/static/js/univers.js
+++ mbc/static/js/univers.js
@@ -12,7 +12,8 @@ $(window).load(function(){
 	bannerHeight = banner.outerHeight();
 	$('#banner-titre').css('left', ($(document).outerWidth() - $('#banner-titre:first').width())/2);
 	$('#banner-titre').css('top', ($('#banner-wrapper').height() - $('#banner-titre:first').outerHeight())/2 -20); // Problème pour centrer le #banner-titre, pour le moment on shorcut avec le -20px...
-	$('#main').css('max-width', $('.container-all-titres').outerWidth()).css('margin-top', $('#banner-wrapper').outerHeight()+'px');
+	$('chiffres').css('margin-top', $('#banner-wrapper').outerHeight()+'px');
+	$('#main').css('max-width', $('.container-all-titres').outerWidth()).css('margin-top', $('#banner-wrapper').outerHeight()+$('#chiffre').outerHeight()+'px');
 });
 
 $(window).resize(function(){
@@ -20,6 +21,7 @@ $(window).resize(function(){
 	bannerHeight = banner.outerHeight();
 	$('#banner-titre').css('left', ($(document).outerWidth() - $('#banner-titre:first').width())/2);
 	$('#banner-titre').css('top', ($('#banner-wrapper').height() - $('#banner-titre:first').outerHeight())/2 -20); // Problème pour centrer le #banner-titre, pour le moment on shorcut avec le -20px...
-	$('#main').css('max-width', $('.container-all-titres').outerWidth()).css('margin-top', $('#banner-wrapper').outerHeight()+'px');
+	$('chiffres').css('margin-top', $('#banner-wrapper').outerHeight()+'px');
+	$('#main').css('max-width', $('.container-all-titres').outerWidth()).css('margin-top', $('#banner-wrapper').outerHeight()+$('#chiffre').outerHeight()+'px');
 });
 
